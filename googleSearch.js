597bb00a6e36ce3c53a991e9ce00db22d9e65a83
Cmendence <chris.mendence@gmail.com>
2023-04-01 14:40:47 -0400

    final week 9 files

--- googleSearch.js
+++ googleSearch.js
@@ -1,6 +1,6 @@
 function searchGoogle() {
-    var query = document.getElementById("searchQuery").value;
-    var url = "https://www.google.com/search?q=" + encodeURIComponent(query);
+    let query = document.getElementById("searchQuery").value;
+    let url = "https://www.google.com/search?q=" + encodeURIComponent(query);
     window.open(url, "_blank");
     document.getElementById("searchQuery").value = "";
   }
